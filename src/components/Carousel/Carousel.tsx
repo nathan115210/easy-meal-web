669581b0a3e1b7f41d3d955cf951dcfddbe5096b
Hongyu.Zhao <nathan_115210@hotmail.com>
2025-07-14 03:11:01 +0300

    Add community page

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,83 +1,72 @@
 'use client';
 
-import {useEffect, useRef, useState} from 'react';
-import CarouselItem, {CarouselItemProps} from './CarouselItem';
+import { useEffect, useRef, useState } from 'react';
+import CarouselItem, { CarouselItemProps } from './CarouselItem';
 import styles from './carousel.module.scss';
 
-const Carousel = ({items}: { items: CarouselItemProps[] }) => {
-    const [current, setCurrent] = useState<number>(0);
-    const timer = useRef<NodeJS.Timeout | null>(null);
-    const itemsLength = items.length;
+const Carousel = ({ items }: { items: CarouselItemProps[] }) => {
+  const [current, setCurrent] = useState<number>(0);
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const itemsLength = items.length;
 
+  const next = () => setCurrent((prev) => (prev + 1) % itemsLength);
 
-    const next = () =>
-        setCurrent((prev) => (prev + 1) % itemsLength);
+  const prev = () => setCurrent((prev) => (prev === 0 ? itemsLength - 1 : prev - 1));
 
-    const prev = () =>
-        setCurrent((prev) =>
-            prev === 0 ? itemsLength - 1 : prev - 1
-        );
+  // reset and start timer
+  const resetTimer = () => {
+    if (timer.current) clearInterval(timer.current);
+    timer.current = setInterval(() => {
+      next();
+    }, 5000);
+  };
 
-    // reset and start timer
-    const resetTimer = () => {
-        if (timer.current) clearInterval(timer.current);
-        timer.current = setInterval(() => {
-            next();
-        }, 5000);
+  useEffect(() => {
+    resetTimer();
+    return () => {
+      if (timer.current) clearInterval(timer.current);
     };
+  }, []); // run once on mount
 
-    useEffect(() => {
-        resetTimer();
-        return () => {
-            if (timer.current) clearInterval(timer.current);
-        };
-    }, []); // run once on mount
+  // whenever current changes by manual click, reset the timer
+  useEffect(() => {
+    resetTimer();
+  }, [current]);
 
-    // whenever current changes by manual click, reset the timer
-    useEffect(() => {
-        resetTimer();
+  const handleMouseEnter = () => {
+    if (timer.current) clearInterval(timer.current);
+  };
 
-    }, [current]);
+  const handleMouseLeave = () => {
+    resetTimer();
+  };
 
-    const handleMouseEnter = () => {
-        if (timer.current) clearInterval(timer.current);
-    }
-
-    const handleMouseLeave = () => {
-        resetTimer();
-    }
-
-    return (
-        <div className={styles.carousel}>
-            {itemsLength > 1 && <button
-                className={styles['carousel__navigation'] + ' ' + styles['carousel__navigation--prev']}
-                onClick={prev}
-                disabled={current === 0}
-                aria-label="Previous"
-            >
-                &#8592;
-            </button>}
-            {itemsLength > 1 && <button
-                className={styles['carousel__navigation'] + ' ' + styles['carousel__navigation--next']}
-                onClick={next}
-                disabled={current === items.length - 1}
-                aria-label="Next"
-            >
-                &#8594;
-            </button>}
-            <div className={styles['carousel__list']} style={{
-                transform: `translateX(-${current * 100}%)`,
-            }} onMouseEnter={handleMouseEnter}
-                 onMouseLeave={handleMouseLeave}>
-
-                {items.map((item, index) => (
-                    <CarouselItem key={index} {...item} />
-                ))}
-            </div>
-        </div>
-
-
-    );
-}
+  return (
+    <div className={styles.carousel}>
+      {itemsLength > 1 && (
+        <button className={styles['carousel__navigation'] + ' ' + styles['carousel__navigation--prev']} onClick={prev} disabled={current === 0} aria-label="Previous">
+          &#8592;
+        </button>
+      )}
+      {itemsLength > 1 && (
+        <button className={styles['carousel__navigation'] + ' ' + styles['carousel__navigation--next']} onClick={next} disabled={current === items.length - 1} aria-label="Next">
+          &#8594;
+        </button>
+      )}
+      <div
+        className={styles['carousel__list']}
+        style={{
+          transform: `translateX(-${current * 100}%)`,
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {items.map((item, index) => (
+          <CarouselItem key={index} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
--- src/constants/homePageData.ts
+++ src/constants/homePageData.ts
@@ -1,4 +1,5 @@
-import type {GridItemProps} from "@/components/Grid/Grid";
+import type {GridItemProps} from '@/components/Grid/Grid';
+import {CarouselItemProps} from "@/components/Carousel/CarouselItem";
 
 export const introSectionContent: {
     heading: string;
@@ -7,23 +8,18 @@ export const introSectionContent: {
     cta: {
         ctaText: string;
         ctaLink: string;
-    }
+    };
 } = {
     heading: 'Fuel Your Day with Easy Meal',
     description: 'Plan, cook, and shop effortlessly with personalized meal recommendations, a dynamic planner, and auto-generated lists.',
-    features: [
-        'Smart Recipe Search',
-        'Weekly Planner & Reminders',
-        'One-Click Shopping Lists',
-        'Community Reviews & Tips'
-    ],
+    features: ['Smart Recipe Search', 'Weekly Planner & Reminders', 'One-Click Shopping Lists', 'Community Reviews & Tips'],
     cta: {
         ctaText: 'Check All Meals',
-        ctaLink: '/meals'
-    }
-}
+        ctaLink: '/meals',
+    },
+};
 
-/*
+
 export const carouselItems: CarouselItemProps[] = [
     {
         title: 'Smart Recipe Search',
@@ -54,7 +50,7 @@ export const carouselItems: CarouselItemProps[] = [
         href: '/learn-more-2',
     },
 ];
-*/
+
 
 export const featureGridItems: GridItemProps[] = [
     {
