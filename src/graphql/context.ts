ba3eb2cb8dbd62e75a107ea4e830de4093023dac
Hongyu.Zhao <nathan_115210@hotmail.com>
2025-08-09 00:27:18 +0300

    feature(DB): migrate to SQL and GraphQL

---
+++ src/graphql/context.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
+
+export type Context = {
+  prisma: PrismaClient;
+};
+
+export function createContext(): Context {
+  return { prisma };
+}
